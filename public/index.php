70db556e7c52ccc10dbb285928687a00db2b1c71
oneilmelisa <melisa@hayesmarketing.io>
2019-02-11 18:24:13 -0600

    ep 18 array start

--- public/index.php
+++ public/index.php
@@ -16,3 +16,24 @@ require 'functions.php';
 require Router::load('routes.php')
 ->direct(Request::uri());
 
+
+
+class Post
+{
+    Public $title;
+    public $published;
+    public function __construct($title, $published)
+    {
+    $this->title = $title;
+    $this->published = $published;
+    }
+}
+
+$posts = [
+    new Post('My 1st Post', true),
+    new Post('My 2nd Post', true),
+    new Post('My 3rd Post', true),
+    new Post('My 4th Post', false)
+
+];
+dd($posts);
